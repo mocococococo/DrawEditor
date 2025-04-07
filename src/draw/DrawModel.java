78eb112fdd671ca2d0965c0fc17ac3c7a2dc3346
mocococococo <tomomichi.kanaeda@paiza.co.jp>
2025-04-07 15:15:33 +0900

    図形の大きさきさ

--- src/draw/DrawModel.java
+++ src/draw/DrawModel.java
@@ -48,12 +48,12 @@ public class DrawModel extends Observable {
     }
     
     public void createFigure(int x, int y) {
-        Fbox[0] = new Rectangle(x, y, x, y, currentColor);
-        Fbox[1] = new FillRectangle(x, y, x, y, currentColor);
-        Fbox[2] = new Line(x, y, x, y, currentColor);
-        Fbox[3] = new Circle(x, y, x, y, currentColor);
-        Fbox[4] = new FillCircle(x, y, x, y, currentColor);
-        Fbox[5] = new Triangle(x, y, x, y, currentColor);
+        Fbox[0] = new Rectangle(x, y, 0, 0, currentColor);
+        Fbox[1] = new FillRectangle(x, y, 0, 0, currentColor);
+        Fbox[2] = new Line(x, y, 0, 0, currentColor);
+        Fbox[3] = new Circle(x, y, 0, 0, currentColor);
+        Fbox[4] = new FillCircle(x, y, 0, 0, currentColor);
+        Fbox[5] = new Triangle(x, y, 0, 0, currentColor);
 
         Figure f = Fbox[this.now.ordinal()];
         fig.add(f);
@@ -64,6 +64,7 @@ public class DrawModel extends Observable {
     
     public void reshapeFigure(int x1, int y1, int x2, int y2) {
         if (drawingFigure != null) {
+            
             drawingFigure.reshape(x1, y1, x2, y2, getFigureType());
             setChanged();
             notifyObservers();
